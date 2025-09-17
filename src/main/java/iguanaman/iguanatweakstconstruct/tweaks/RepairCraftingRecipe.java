defe7020674ad8d5b1cfcbc510131bf381743c58
Jordan Rey <rey.jordan.1998@gmail.com>
2025-09-17 13:17:44 +0200

    fix: make the easy tool repair compatible with TGregworks (#23)

--- src/main/java/iguanaman/iguanatweakstconstruct/tweaks/RepairCraftingRecipe.java
+++ src/main/java/iguanaman/iguanatweakstconstruct/tweaks/RepairCraftingRecipe.java
@@ -6,11 +6,14 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.RecipeSorter;
 
+import cpw.mods.fml.common.Optional;
 import iguanaman.iguanatweakstconstruct.reference.Reference;
+import iguanaman.iguanatweakstconstruct.util.ModSupportHelper;
 import tconstruct.library.crafting.ModifyBuilder;
 import tconstruct.library.modifier.ItemModifier;
 import tconstruct.library.tools.ToolCore;
 import tconstruct.modifiers.tools.ModToolRepair;
+import vexatos.tgregworks.integration.modifiers.ModTGregRepair;
 
 public class RepairCraftingRecipe implements IRecipe {
 
@@ -23,18 +26,32 @@ public class RepairCraftingRecipe implements IRecipe {
                 "");
     }
 
-    private ModToolRepair modifier = null;
+    private ItemModifier modifier = null;
     private ItemStack modifiedTool = null;
 
     public RepairCraftingRecipe() {
-        for (ItemModifier mod : ModifyBuilder.instance.itemModifiers) if (mod instanceof ModToolRepair) {
-            modifier = (ModToolRepair) mod;
-            break;
+        for (ItemModifier mod : ModifyBuilder.instance.itemModifiers) {
+            // If TGregworks is present, use ModTGregRepair instead of ModToolRepair
+            if (ModSupportHelper.TGregworks && isTGregRepairModifier(mod)) {
+                modifier = mod;
+                break;
+            } else if (mod instanceof ModToolRepair) {
+                modifier = mod;
+                break;
+            }
         }
+    }
+
+    @Optional.Method(modid = "TGregworks")
+    private boolean isTGregRepairModifier(ItemModifier mod) {
+        return mod instanceof ModTGregRepair;
     }
 
     @Override
     public boolean matches(InventoryCrafting inventoryCrafting, World world) {
+        // if no compatible repair modifier was found during init, this recipe can't work
+        if (modifier == null) return false;
+
         ItemStack tool = null;
         ItemStack[] input = new ItemStack[inventoryCrafting.getSizeInventory()];
 
